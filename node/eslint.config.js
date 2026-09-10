c5beadf779e2aeef6bc0565cbae6cd468c1a73cf
Rumit Parakhiya <rumit.parakhiya@tarktech.com>
2026-09-10 17:19:26 +0530

    Added lint in the workflow.

---
+++ node/eslint.config.js
@@ -0,0 +1,13 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: globals.node,
+    },
+  },
+];
